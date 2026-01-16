2085ebcb0d9fe74a81d023a90df0d38b881f35b9
becarolf <becarolf@gmail.com>
2026-01-16 13:56:24 -0500

    add lab 01 answers

--- Week01/lab1.py
+++ Week01/lab1.py
@@ -4,3 +4,18 @@
 
 # Question 2: Defining an array (list)
 my_array = [1, 4, 7, 9]
+
+
+# Question 3: Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully bracketed version
+e = (a - ((b ** c) // d)) + (a % c)
+
+
+# Question 4: Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
